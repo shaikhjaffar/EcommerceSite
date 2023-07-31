823610aaa2ad16310149706a3abc82c8ed6a6ed0
[firstname <jaffar.shaikh@1clickcapital.com>
2023-07-31 11:08:03 +0530

    Added_persist

--- src/components/redux/store.js
+++ src/components/redux/store.js
@@ -2,7 +2,18 @@
 import {legacy_createStore as createStore,applyMiddleware} from 'redux'
 import thunk from 'redux-thunk';
 import rootReducer from './reducer/index';
+import storage from 'redux-persist/lib/storage';
+import { persistReducer} from 'redux-persist';
 
-const store = createStore(rootReducer,applyMiddleware(thunk))
+
+const persistConfig = {
+    key: 'root',
+    storage,
+  }
+
+
+  const persisteducer = persistReducer(persistConfig,rootReducer)
+
+const store = createStore(persisteducer,applyMiddleware(thunk))
 
 export default store;
--- src/index.js
+++ src/index.js
@@ -6,13 +6,18 @@ import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './components/redux/store';
-
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistStore } from 'redux-persist';
 const root = ReactDOM.createRoot(document.getElementById('root'));
+ let persistor = persistStore(store)
 root.render(
   <Provider store={store}>
   <React.StrictMode>
     <BrowserRouter>
+    <PersistGate persistor={persistor}>
     <App />
+    </PersistGate>
+   
     </BrowserRouter>
    
   </React.StrictMode>
