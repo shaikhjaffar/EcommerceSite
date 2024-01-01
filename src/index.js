863ba822dd987b92d938a68fbe07416908912425
Mohammed Jaffar SHAIKH <92439144+shaikhjaffar@users.noreply.github.com>
2024-01-01 11:35:23 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 
 // import store from './redux/store';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './components/redux/store';
 import { PersistGate } from 'redux-persist/integration/react';
@@ -13,12 +13,12 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
   <React.StrictMode>
-    <BrowserRouter>
+   <HashRouter>
     <PersistGate persistor={persistor}>
     <App />
     </PersistGate>
-   
-    </BrowserRouter>
+   </HashRouter>
+    
    
   </React.StrictMode>
   </Provider>
